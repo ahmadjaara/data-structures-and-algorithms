7d89403003c09c397bd50373f2264fd887ac98fd
ahmadjaara <ahmadjaara98@yahoo.com>
2022-04-11 23:18:55 +0300

    max

--- Tree/tests/test_tree.py
+++ Tree/tests/test_tree.py
@@ -1,4 +1,6 @@
 from tree.tree import TNode, Binary_Search_Tree,BinaryTree
+from tree.new_stack import Node,Stack
+import pytest
 
 def test_empty_tree():
     tree = BinaryTree()
@@ -19,37 +21,17 @@ def test_bst_add_left_right_node():
     assert tree.root.left.value==1
     assert tree.root.right.value==4
 
-def test_preorder():
-    node1 = TNode(1)
-    node2 = TNode(2)
-    node3 = TNode(3)
-    node4 = TNode(4)
-    node1.left = node2
-    node1.right = node3
-    node3.right = node4
-    tree = BinaryTree()
-    assert tree.pre_order==tree.pre_order
+def test_preorder(tree):
 
-def test_inorder():
-    node1 = TNode(1)
-    node2 = TNode(2)
-    node3 = TNode(3)
-    node4 = TNode(4)
-    node1.left = node2
-    node1.right = node3
-    node3.right = node4
-    tree = Binary_Search_Tree()
-    assert tree.in_order==tree.in_order
-def test_postorder():
-    node1 = TNode(1)
-    node2 = TNode(2)
-    node3 = TNode(3)
-    node4 = TNode(4)
-    node1.left = node2
-    node1.right = node3
-    node3.right = node4
-    tree = Binary_Search_Tree()
-    assert tree.post_order==tree.post_order
+    assert tree.pre_order() == ['1', '2', '3', '4']
+
+def test_inorder(tree):
+
+    assert tree.in_order()==['2', '1', '3', '4']
+
+def test_postorder(tree):
+
+    assert tree.post_order()==['2', '4', '3', '1']
 
 def test_bst_cotains_method():
     node1 = TNode(2)
@@ -58,3 +40,42 @@ def test_bst_cotains_method():
     tree.add(1)
     assert tree.contains(4) == False
     assert tree.contains(1) == True
+def test_max_method1(tree):
+    assert tree.max_value() == 4
+
+def test_max_method2(tree2):
+    assert tree2.max_value() == 11
+
+@pytest.fixture
+def tree():
+    node1 = TNode(1)
+    node2 = TNode(2)
+    node3 = TNode(3)
+    node4 = TNode(4)
+    node1.left = node2
+    node1.right = node3
+    node3.right = node4
+    tree = Binary_Search_Tree()
+    tree.root=node1
+    return tree
+@pytest.fixture
+def tree2():
+    node1 = TNode(2)
+    node2 = TNode(7)
+    node3 = TNode(5)
+    node4 = TNode(2)
+    node5 = TNode(6)
+    node6 = TNode(5)
+    node7 = TNode(11)
+    node8 = TNode(9)
+    node1.left = node2
+    node1.right = node3
+    node3.right = node8
+    node2.left=node4
+    node2.right=node5
+    node5.left=node6
+    node5.right=node7
+
+    tree = Binary_Search_Tree()
+    tree.root=node1
+    return tree
--- Tree/tree/tree.py
+++ Tree/tree/tree.py
@@ -1,3 +1,47 @@
+# class Node:
+#     def __init__(self,value):
+#         self.value=value
+#         self.next=None
+# class Stack:
+
+#     def __init__(self):
+#         self.top=None
+
+#     def push(self, value):
+#         """
+#             push will add a new Node to the stack
+
+#             input: value
+#             output: None
+
+#         """
+#         node = Node(value)
+#         node.next = self.top
+#         self.top = node
+
+#     def pop(self):
+#         """
+#         input: none
+#         doing: pop the top node from the stack 
+#         output: popped node's value
+#         """
+#         if self.is_empty():
+#             raise Exception("Stack is empty !")
+        
+#         temp=self.top
+#         self.top=self.top.next
+#         temp.next=None
+        
+#         return temp.value
+        
+        
+#     def is_empty(self):
+#         if self.top == None:
+#             return True 
+#         return False
+
+from tree.new_stack import Stack,Node
+
 
 class TNode:
     def __init__(self,value):
@@ -16,10 +60,11 @@ class BinaryTree:
         doing: traverse a tree
         output: print values of the nodes of the tree Pre-order: root >> left >> right
         """
-        
+        list_pre=[]
         def _walk(node):
             
-            print(node.value)
+            #print(str(node.value) + "->", end='')
+            list_pre.append(str(node.value))
 
             if node.left:
                 _walk(node.left)
@@ -28,7 +73,7 @@ class BinaryTree:
         
         _walk(self.root)
         
-    
+        return list_pre
 
         # stack = Stack()
         # current = self.root
@@ -50,22 +95,26 @@ class BinaryTree:
         doing: traverse a tree
         output: print values of the nodes of the tree In-order: left >> root >> right
         """
+        list_in_or=[]
         def _walk(node):
             
-
+            
             if node.left:
                 _walk(node.left)
-            print(node.value)
+            #print(str(node.value) + "->", end='')
+            list_in_or.append(str(node.value))
             if node.right:
                 _walk(node.right)
 
         _walk(self.root)
+        return list_in_or
     def post_order(self):
         """
         Input: None
         doing: traverse a tree
         output: print values of the nodes of the tree **Post-order: left >> right >> root
         """
+        list_post_or=[]
         def _walk(node):
             
             if node.left:
@@ -73,10 +122,35 @@ class BinaryTree:
             
             if node.right:
                 _walk(node.right)
-            
-            print(node.value)
+            list_post_or.append(str(node.value))
+            #print(str(node.value) + "->", end='')
         _walk(self.root)
+        return list_post_or
         
+    def max_value(self):
+        """
+        Find the maximum value stored in the tree
+        Arguments: none
+        Returns: number
+        """
+        stack = Stack()
+        current = self.root
+        maxv=current.value
+
+        stack.push(current)
+
+        while not stack.is_empty():
+            current = stack.pop()
+            if maxv <= current.value:
+                maxv=current.value
+            # print(current.value)
+
+            if current.right:
+                stack.push(current.right)
+
+            if current.left:
+                stack.push(current.left)
+        return maxv
 
 class Binary_Search_Tree(BinaryTree):
     
@@ -125,7 +199,7 @@ class Binary_Search_Tree(BinaryTree):
                 ptr_node = ptr_node.left
             else:
                 ptr_node = ptr_node.right
-        
+    
 
 
 if __name__=="__main__":
@@ -143,7 +217,8 @@ if __name__=="__main__":
     
     # tree.add(6)
     # print(tree.contains(4))
-    print(tree.post_order())
+    # print(tree.post_order())
     # print("1\n2\n3\n4")
     # print(tree.root.left.value)
+    print(tree.max_value())
       
